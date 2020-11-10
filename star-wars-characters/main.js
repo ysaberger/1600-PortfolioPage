614816d989eeae8b39e27cf7ffb22ca217916302
ysaberger <ysaberger@gmail.com>
2020-11-10 14:58:01 -0700

    almost done

--- star-wars-characters/main.js
+++ star-wars-characters/main.js
@@ -16,28 +16,72 @@ mainHeader.appendChild(maleButton)
 
 const otherButton = document.createElement('button')
 otherButton.textContent = 'Other Characters'
-mainHeader.appendChild(maleButton)
+mainHeader.appendChild(otherButton)
 
 
+const maleCharacters = people.filter((person) => person.gender === 'male')
+console.log(maleCharacters)
 
-const maleCharacters = people.filter(person => person.gender === 'male')
-
-maleButton.addEventListener('click', event => {
-    maleCharacters.forEach(element) => {
-    const charFigure = document.createElement('figure')
-    const charImg = document.createElement('img')
+maleButton.addEventListener('click', (event) => {
+    maleCharacters.forEach((element) => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
     let charNum = getLastNumber(element.url)
-    charImg.src = 'https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg'
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+    charImg.addEventListener('error', () => cgarImg.hidden = true)
     const charCaption = document.createElement('figcaption')
     charCaption.textContent = element.name
 
     charFigure.appendChild(charImg)
     charFigure.appendChild(charCaption)
-    }
+
+    mainContent.appendChild(charFigure)
+    })
 })
 
-let theUrl = "https://swapi.co/api/people/2/"
-let theUrl2 = "https://swapi.co/api/people/"
+
+const femaleCharacters = people.filter((person) => person.gender === 'female')
+console.log(femaleCharacters)
+
+femaleButton.addEventListener('click', (event) => {
+    femaleCharacters.forEach((element) => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+    let charNum = getLastNumber(element.url)
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+    charImg.addEventListener('error', () => cgarImg.hidden = true)
+    const charCaption = document.createElement('figcaption')
+    charCaption.textContent = element.name
+
+    charFigure.appendChild(charImg)
+    charFigure.appendChild(charCaption)
+
+    mainContent.appendChild(charFigure)
+    })
+})
+
+
+const otherCharacters = people.filter((person) => person.gender === 'other')
+console.log(otherCharacters)
+
+otherButton.addEventListener('click', (event) => {
+    otherCharacters.forEach((element) => {
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+    let charNum = getLastNumber(element.url)
+    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+    charImg.addEventListener('error', () => cgarImg.hidden = true)
+    const charCaption = document.createElement('figcaption')
+    charCaption.textContent = element.name
+
+    charFigure.appendChild(charImg)
+    charFigure.appendChild(charCaption)
+
+    mainContent.appendChild(charFigure)
+    })
+})
+//let theUrl = "https://swapi.co/api/people/2/"
+//let theUrl2 = "https://swapi.co/api/people/"
 
 function getLastNumber(url) {
 let end = url.lastIndexOf('/')
