f641b6ccc5ed8d7c2ee0443946af92ad32630174
ysaberger <ysaberger@gmail.com>
2020-11-09 22:00:43 -0700

    commit

--- star-wars-api/main.js
+++ star-wars-api/main.js
@@ -8,11 +8,28 @@ import { people } from './data/films.js'
     newParagraph.textContent = person.name
 })*/
 
-for (const film of films) {
-    let newImg = document.createElement('img')
-    newImg.src = 
-    console.log(film.title);
+
+const main = document.querySelector('main')
+
+for (let step = 0; step < 7; step++) {
+    //Runs 5 times, with values of step 0 through 6.
+    let newImg = document.createElement('img') // new image yo
+    newImg.src = 'http://starwars-visualguide.com/assets/img/films/' + (step +1) + 
+'.jpg' //source
+//image appended
+    main.appendChild(newImg)
+    console.log(films[step].title);
 }
+
+/*for (const film of films) {
+    
+}*/
+
+/*people.forEach(person => {
+let newParagraph = document.body.appendChild(document.createElement('p'))
+newParagraph.textContent = person.name
+})*/
+
 /*films.forEach(film => {
     let newParagraph = document.body.appendChild(document.createElement('p'))
     newParagraph.textContent = film.opening_crawl
