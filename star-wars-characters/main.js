3a29b6561ddb351ff25699a69ccace0fc722bedb
ysaberger <ysaberger@gmail.com>
2020-11-10 15:57:54 -0700

    commit

--- star-wars-characters/main.js
+++ star-wars-characters/main.js
@@ -20,66 +20,36 @@ mainHeader.appendChild(otherButton)
 
 
 const maleCharacters = people.filter((person) => person.gender === 'male')
-console.log(maleCharacters)
-
-maleButton.addEventListener('click', (event) => {
-    maleCharacters.forEach((element) => {
-        const charFigure = document.createElement('figure')
-        const charImg = document.createElement('img')
-    let charNum = getLastNumber(element.url)
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
-    charImg.addEventListener('error', () => cgarImg.hidden = true)
-    const charCaption = document.createElement('figcaption')
-    charCaption.textContent = element.name
-
-    charFigure.appendChild(charImg)
-    charFigure.appendChild(charCaption)
-
-    mainContent.appendChild(charFigure)
-    })
-})
-
 
 const femaleCharacters = people.filter((person) => person.gender === 'female')
-console.log(femaleCharacters)
 
-femaleButton.addEventListener('click', (event) => {
-    femaleCharacters.forEach((element) => {
-        const charFigure = document.createElement('figure')
-        const charImg = document.createElement('img')
-    let charNum = getLastNumber(element.url)
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
-    charImg.addEventListener('error', () => cgarImg.hidden = true)
-    const charCaption = document.createElement('figcaption')
-    charCaption.textContent = element.name
+const otherCharacters = people.filter((person) => (person.gender != 'male' && person.gender != 'female'))
 
-    charFigure.appendChild(charImg)
-    charFigure.appendChild(charCaption)
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
 
-    mainContent.appendChild(charFigure)
-    })
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
+
+otherButton.addEventListener('click', () => populateDOM(otherCharacters))
+
+
+function populateDOM(characters) {
+    removeChildren(mainContent)
+characters.forEach((element) => {
+    const charFigure = document.createElement('figure')
+    const charImg = document.createElement('img')
+let charNum = getLastNumber(element.url)
+charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+charImg.addEventListener('error', () => charImg.hidden = true)
+const charCaption = document.createElement('figcaption')
+charCaption.textContent = element.name
+
+charFigure.appendChild(charImg)
+charFigure.appendChild(charCaption)
+
+mainContent.appendChild(charFigure)
 })
+}
 
-
-const otherCharacters = people.filter((person) => person.gender === 'other')
-console.log(otherCharacters)
-
-otherButton.addEventListener('click', (event) => {
-    otherCharacters.forEach((element) => {
-        const charFigure = document.createElement('figure')
-        const charImg = document.createElement('img')
-    let charNum = getLastNumber(element.url)
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
-    charImg.addEventListener('error', () => cgarImg.hidden = true)
-    const charCaption = document.createElement('figcaption')
-    charCaption.textContent = element.name
-
-    charFigure.appendChild(charImg)
-    charFigure.appendChild(charCaption)
-
-    mainContent.appendChild(charFigure)
-    })
-})
 //let theUrl = "https://swapi.co/api/people/2/"
 //let theUrl2 = "https://swapi.co/api/people/"
 
@@ -89,4 +59,10 @@ let start = end - 2
 if (url.charAt(start) === '/') { start++
 }
 return url.slice(start, end)
+}
+
+function removeChildren(container) {
+    while (container.firstChild) { 
+container.removeChild(container.firstChild)
+}
 }
