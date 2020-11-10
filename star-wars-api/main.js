8e454315a4be6ad45ac419edeb97c94947065d18
ysaberger <ysaberger@gmail.com>
2020-11-09 21:25:41 -0700

    commit

--- star-wars-api/main.js
+++ star-wars-api/main.js
@@ -1,13 +1,18 @@
 import { films } from './data/films.js'
+import { people } from './data/films.js'
 
 //console.log(films[0])
 
-people.forEach(person) => {
+/*people.forEach(person) => {
     let newParagraph = document.body.appendChild(document.createElement('p'))
     newParagraph.textContent = person.name
-})
+})*/
 
-
+for (const film of films) {
+    let newImg = document.createElement('img')
+    newImg.src = 
+    console.log(film.title);
+}
 /*films.forEach(film => {
     let newParagraph = document.body.appendChild(document.createElement('p'))
     newParagraph.textContent = film.opening_crawl
