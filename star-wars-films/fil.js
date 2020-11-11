fd8fb8aec78eaf91c3e439802537b3e2897fa523
ysaberger <ysaberger@gmail.com>
2020-11-10 19:48:06 -0700

    commit

--- star-wars-films/fil.js
+++ star-wars-films/fil.js
@@ -2,27 +2,34 @@ import { films } from '../data/films.js'
 
 const main = document.querySelector('main')
 
-for (let step = 0; step < 7; step++) {
+for (let film of films) {
     let figure = document.createElement('figure')
     let figImg = document.createElement('img')
-    figImg.src = `https://starwars-visualguide.com/assets/img/films/${step + 1}.jpg`
     let figCaption = document.createElement('figcaption')
-    figCaption.textContent = films[step].title
+    figCaption.textContent = film.title
+
+    var episode_id = [1, 2, 3, 4, 5, 6, 7];
+episode_id.sort(function(a, b){return a-b});
+
+    if (film.episode_id === 1) {
+        figImg.src = `https://starwars-visualguide.com/assets/img/films/4.jpg`
+    } else if (film.episode_id === 5) {
+        figImg.src = `https://starwars-visualguide.com/assets/img/films/2.jpg`
+         }   else if (film.episode_id === 4) {
+            figImg.src = `https://starwars-visualguide.com/assets/img/films/1.jpg`
+        } 
+    else if (film.episode_id === 6) {
+        figImg.src = `https://starwars-visualguide.com/assets/img/films/3.jpg`
+    } else if (film.episode_id === 3) {
+        figImg.src = `https://starwars-visualguide.com/assets/img/films/4.jpg`
+    } else if (film.episode_id === 2) {
+        figImg.src = `https://starwars-visualguide.com/assets/img/films/5.jpg`
+    }  else if (film.episode_id === 7) {
+        figImg.src = `https://starwars-visualguide.com/assets/img/films/7.jpg`
+    }
 
     figure.appendChild(figImg)
     figure.appendChild(figCaption)
 
     main.appendChild(figure)
 }
-
-/*people.forEach(person => {
-let newParagraph = document.body.appendChild(document.createElement('p'))
-newParagraph.textContent = person.name
-})*/
-
-/*films.forEach(film => {
-    let newParagraph = document.body.appendChild(document.createElement('p'))
-    newParagraph.textContent = film.opening_crawl
-})*/
-
-/* <img src= "/images/star_wars_logo_PNG40.png"> */
