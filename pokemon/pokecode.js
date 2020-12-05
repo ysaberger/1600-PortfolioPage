5f20cab396276996601f802ed4a7dbdfde523488
ysaberger <ysaberger@gmail.com>
2020-12-05 13:48:24 -0700

    commit

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -8,3 +8,28 @@ try {
 }
 
 }
+
+//example
+function loadPage () {
+    getAPIData(`https://pokeapi.co/api/v2/pokemon`).then
+    (async (data) => {
+for (const pokemon of data.results) {
+    await getAPIData(pokemon.url).then((pokeData) => {
+        populatePokeCard(pokeData)
+    })
+}
+    })
+}
+
+const pokemonGrid = pocument.querySelector('.pokemonGrid')
+
+function populatePokeCard(pokemon) {
+    let cardFront = document.createElement('div')
+    let frontLabel = document.createElement('p')
+
+    frontLabel.textConent = pokemon.name
+    cardFront.appendChild(frontLabel)
+    pokemonGrid.appendChild(cardFront)
+}
+
+loadPage()
