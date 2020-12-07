b82ed06e4bc932f4765cf251e36a518ba0c48378
ysaberger <ysaberger@gmail.com>
2020-12-06 21:43:17 -0700

    commit

--- newPokeSite/pokecode.js
+++ newPokeSite/pokecode.js
@@ -1,15 +1,5 @@
-//Reusable function to fetch data
-async function getAPIData(url) {
-try {
-  const respond = await fetch(url)
-  const data = await response.json()
-} catch (error) {
- console.error(error)
-}}
-
-//cards loading with names
 async function loadCards() {
-    let apiResults = await getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25&offset=0`);
+    let apiResults = await getAPIData(`https://pokeapi.co/api/v2/pokemon`);
     //console.log(apiResults)
     for(let entry of apiResults) {
         console.log(entry.name);
@@ -37,5 +27,6 @@ function populatePokeCard(pokemon) {
     <p id="${pokemon}CardTitle" class="cardText">${pokemon} </p> 
     </div>`
 }
+
 
 document.body.onload = loadCards;
