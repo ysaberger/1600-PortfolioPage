0cfb019d0e13990746e41b7a0729a9294d9aad12
ysaberger <ysaberger@gmail.com>
2020-12-08 02:17:25 -0700

    commit

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,41 +1,49 @@
 //Reusable function to fetch data
 async function getAPIData(url) {
-try {
-  const respond = await fetch(url)
-  const data = await response.json()
-} catch (error) {
- console.error(error)
-}}
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      return data
+    } catch (error) {
+     console.error(error)
+    }}
 
 //cards loading with names
-async function loadCards() {
-    let apiResults = await getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25&offset=0`);
+function loadPage() {
+  getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25&offset=0`).then
     //console.log(apiResults)
-    for(let entry of apiResults) {
-        console.log(entry.name);
-        populatePokeCard(entry.name);
+    (async (data) => {
+        for(const pokemon of data.results) {
+            await getAPIData(pokemon.url).then((pokeData) => {
+            populatePokeCard(pokeData)
+        })
     }
+    })
 }
 
-async function getAPIData(url) {
-    try {
-        let output;
-        await fetch(url)
-        .then(response => response.json())
-        .then(data => output = data.results)
-        //.then(() => console.log(output));
-        return output;
-    } catch (error) {
-        console.error(error);
-    }
-}
+const pokemonGrid = document.querySelector('.pokemonGrid')
 
 function populatePokeCard(pokemon) {
-    let parentDiv = document.getElementById("pokemonGrid");
-    parentDiv.innerHTML += `
-    <div id="${pokemon}Card" class="card"> 
-    <p id="${pokemon}CardTitle" class="cardText">${pokemon} </p> 
-    </div>`
+    let pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    let pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    let cardFront = document.createElement('div')
+    let frontLabel = document.createElement('p')
+    let frontImage = document.createElement('img')
+    let cardBack = document.createElement('div')
+    let backLabel = document.createElement('p')
+
+    frontLabel.textContent = pokemon.name
+    frontImage.src = `./images/00${pokemon.id}.png`
+    backLabel.textContent = `Say hello to the back`
+    cardFront.appendChild(frontImage)
+    cardFront.appendChild(frontLabel)
+    cardBack.appendChild(backLabel)
+    pokeCard.appendChild(cardFront)
+    pokeCard.appendChild(cardBack)
+    pokeScene.appendChild(pokeCard)
+    pokemonGrid.appendChild(pokeScene)
 }
 
-document.body.onload = loadCards;
+loadPage()
