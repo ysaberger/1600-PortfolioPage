b0e5d96a75dd99314560166e68d1bfc004771aed
ysaberger <ysaberger@gmail.com>
2020-12-15 00:05:20 -0700

    commit

--- pokemon/pokecode.js
+++ pokemon/pokecode.js
@@ -1,126 +1,93 @@
-//Reusable function to fetch data
+//example api async function
 async function getAPIData(url) {
     try {
-      const response = await fetch(url)
-      const data = await response.json()
-      return data
+        let output;
+        await fetch(url)
+        .then(response => response.json())
+        .then(data => output = data.results)
+        //.then(() => console.log(output));
+        return output;
     } catch (error) {
-     console.error(error)
-    }}
-
-//you almost can add more cards!!!! you can do it!!
-const pokemonGrid = document.querySelector('.pokemonGrid')
-const loadButton = document.querySelector('button')
-const newButton = document.querySelector('#newPokemon')
-
-loadButton.addEventListener('click', () => {
-    loadPage()
-    loadButton.diabled = true
-})
-
-newButton.addEventListener('click', () =>  {
-    let pokeName = prompt("What's your new Pokemon's name")
-    populatePokeCard(createNewPokemon(pokeName))
-})
-
-function Pokemon(name, abilities, move){
-    this.name = name
-    this.abilities = abilities
-    this.move = move
-
-}
-function createNewPokemon(name) {
-  return new Pokemon(name, 450, 200, ['gorge', 'sleep', 'cough'], ['thunder', 'dance'])
+        console.error(error);
+    }
 }
 
-function removeChildren(container) {
-    while (container.firstChild) { 
-container.removeChild(container.firstChild)
-}
-}
+//ES6- proper use of let and const variables
+let pokemonData = [];
+let pageSet = true;
+let pokemonCount = 25;
 
-//cards loading with names
-function loadPage() {
-  getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=25&offset=0`).then
+async function loadCards() {
+    let apiResults = await getAPIData(`https://pokeapi.co/api/v2/pokemon?limit=${pokemonCount}`);
     //console.log(apiResults)
-    (async (data) => {
-        for(const pokemon of data.results) {
-            await getAPIData(pokemon.url).then((pokeData) => {
-            populatePokeCard(pokeData)
-        })
+    for(let entry of apiResults) {
+        //console.log(entry.name);
+        let stats = await getPokemonStats(entry);
+        let newPokemon = {
+            name: entry.name,
+            url: entry.url,
+            stats: stats
+        }
+        pokemonData.push(newPokemon);
+        populatePokeCard(newPokemon);
     }
-    })
+    pageSet = false;
 }
-
+//
 function populatePokeCard(pokemon) {
-    let pokeScene = document.createElement('div')
-    pokeScene.className = 'scene'
-    let pokeCard = document.createElement('div')
-    pokeCard.className = 'card'
-    pokeCard.addEventListener('click', () => {
-        pokeCard.classList.toggle('is-flipped')
-    })
+    let parentDiv = document.getElementById("pokemonGrid");
+    parentDiv.innerHTML += `
+    <div class="scene">
+        <div id="${pokemon.name}Card" class="card">
+            <div id="${pokemon.name}Front" class="card-face card-is-front">
+                <p id="${pokemon.name}CardTitle" class="cardText">${pokemon.name} </p>
+                <img id="${pokemon.name}Image" class="image" src="./images/${pokemon.name}.png">
+            </div>
+            <div id="${pokemon.name}Back" class="card-face card-is-back">
+                <p id="${pokemon.name}Stats" class="cardText"> Stats </p>
+                <p class="statText"> ${pokemon.stats[0]} </p>
+                <p class="statText"> ${pokemon.stats[1]} </p>
+                <p class="statText"> ${pokemon.stats[2]} </p>
+                <p class="statText"> ${pokemon.stats[3]} </p>
+                <p class="statText"> ${pokemon.stats[4]} </p>
+                <p class="statText"> ${pokemon.stats[5]} </p>
+            </div>
+        </div>
+    </div>`
 
-    pokeCard.appendChild(populateCardFront(pokemon))
-    pokeCard.appendChild(populateCardBack(pokemon))
-
-    pokeScene.appendChild(pokeCard)
-    pokemonGrid.appendChild(pokeScene)
 }
 
-function populateCardFront(pokemon) {
-    let cardFront = document.createElement('div')
-    cardFront.className = `card__face card__face--front`
-    let frontLabel = document.createElement('p')
-    let frontImage = document.createElement('img')
-    frontLabel.textContent = pokemon.name
-    frontImage.src = `./images/${getImageFileName(pokemon)}.png`
-    cardFront.appendChild(frontImage)
-    cardFront.appendChild(frontLabel)
-    return cardFront
+async function getPokemonStats(pokemon) {
+    await fetch(pokemon.url)
+    .then(response => response.json())
+    .then(data => output = data.stats);
+    //console.log(output);
+    return output.map((item) => {
+        //console.log(item);
+        return `${item.stat.name}: ${item.base_stat}`;
+    });
 }
 
-
-function populateCardBack(pokemon){
-    let cardBack = document.createElement('div')
-    cardBack.className = 'card__face card__face--back'
-    let backLabel = document.createElement('h3')
-    backLabel.textContent = 'Abilities:'
-    let abilityList = document.createElement('ul')
-   // I've tried so hard to fix this bug but nothing works :'(
-    pokemon.abilities.forEach(ability => {
-        let abilityName = document.createElement('li')
-        abilityName.textContent = ability.ability.name
-        abilityList.appendChild(abilityName)
-    })
-    let movesLabel = document.createElement('h4')
-    movesLabel.textContent = 'Most Accurate Move:'
-    let moveAccuracy = document.createElement('h5')
-    const mostAccurateMove = getBestAccuracy(pokemon.moves)
-    console.log(mostAccurateMove.move)
-    moveAccuracy.textContent = `${mostAccurateMove.move.name}`
-    cardBack.appendChild(backLabel)
-    cardBack.appendChild(abilityList)
-    cardBack.appendChild(movesLabel)
-    cardBack.appendChild(moveAccuracy)
-    return cardBack
-}
-
-function getBestAccuracy(pokemoves) {
-    return pokemoves.reduce((mostAccurate, move) => {
-        console.log(move)
-        return mostAccurate.accuracy > move.accuracy ? mostAccurate : move;
-    }, {});
-}
-function getImageFileName(pokemon) {
-    if (pokemon.id < 10) {
-        return `00${pokemon.id}`
-    } else if (pokemon.id > 9 && pokemon.id < 99) {
-        return `0${pokemon.id}`
+document.getElementById("hoverArea").addEventListener('mouseover', () => {
+    if (!pageSet) {
+        console.log("set");
+        pageSet = true;
+        for(let pokemon of pokemonData) {
+            //console.log(pokemon.name);
+            let card = document.getElementById(`${pokemon.name}Card`);
+            card.addEventListener( 'click', function() {
+                card.classList.toggle('is-flipped');
+            });
+        }
     }
-}
-
-loadPage()
+});
+document.getElementById("pokemonCount").addEventListener('change', function() {
+    pokemonCount = document.getElementById("pokemonCount").value;
+    document.getElementById("pokemonGrid").innerHTML = "";
+    pokemonData = [];
+    loadCards();
+})
+document.body.onload = loadCards;
 
 //theme song music player functions
 
